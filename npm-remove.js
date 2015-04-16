04b1e34938de38e696106e5e2d5af9683963feba
maple-leaf <tangjf.88@163.com>
2015-04-16 16:09:17 +0800

    don't remove package.json config

--- npm-remove.js
+++ npm-remove.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var packages = require('./package.json');
 
 Object.keys(packages.devDependencies).forEach(function(key) {
-    exec('npm remove --save-dev ' + key, function(err, stdout, stderr) {
+    exec('npm remove ' + key, function(err, stdout, stderr) {
         console.log(stdout);
     });
 });
